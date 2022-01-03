7280b893e7de5ebec1bfe2dbc9bce0d94739a2e7
mySpaceHere123 <ujjwal.raj.5798@gmail.com>
2022-01-04 04:51:47 +0530

    scrapped

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CodechefPanel from "./components/CodechefPanel";
+
+function App() {
+  return (
+    <div className="App">
+      <CodechefPanel />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/CodechefPanel.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { codechefScrapper } from "../scrapper/codechef";
+
+const CodechefPanel = () => {
+  const codechefData = codechefScrapper();
+
+  return <div></div>;
+};
+
+export default CodechefPanel;
